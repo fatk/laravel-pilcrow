2834cc531118114fe36ab5f55dd2073fd43ddc38
Karl Fathi <karl.fathi@gmail.com>
2024-11-05 17:44:36 -0300

    Add: User importer

--- src/Importers/UserImporter.php
+++ src/Importers/UserImporter.php
@@ -6,6 +6,7 @@ namespace Fatk\Pilcrow\Importers;
 
 use Fatk\Pilcrow\Contracts\ImportTypeInterface;
 use Illuminate\Support\Collection;
+use Fatk\Pilcrow\Helpers\User;
 
 /**
  * Handles the import of WordPress users
@@ -16,23 +17,75 @@ use Illuminate\Support\Collection;
  */
 final class UserImporter implements ImportTypeInterface
 {
+
+    private static ?Collection $roles;
+
+    public function __construct()
+    {
+        self::$roles ??= collect(wp_roles()?->roles)->keys();
+    }
+
     /**
      * Import users from processed data
-     *
-     * Expected data structure:
-     * [
-     *     'user_login' => string,
-     *     'user_email' => string,
-     *     'user_pass' => string,
-     *     'role' => string,
-     *     'meta' => array<string, mixed>,
-     *     'send_notification' => bool
-     * ]
      *
      * @inheritDoc
      */
     public function import(Collection $data): Collection
     {
-        return collect();
+        if (!blank($data->get('user_login'))) {
+            $user = new User($data->get('user_login'));
+
+            $this->processFields($data, $user);
+
+            $user->set($data->filter());
+            $status = $user->save();
+        }
+
+        return collect([
+            'id' => $user?->find()?->ID ?? 'N/A',
+            'login' => $data->get('user_login'),
+            'display name' => $data->get('display_name'),
+            'role' => $data->get('role'),
+            'status' => $status ?? User::STATUS['SAVE_SKIPPED']
+        ]);
+    }
+
+    /**
+     * Process fields requiring special handling
+     *
+     * @param Collection<string, mixed> $data
+     * @param User $user
+     */
+    private function processFields(Collection $data, User $user): void
+    {
+        if (!blank($data->get('seo_title'))) {
+            $user->setSeo(
+                title: $data->get('seo_title'),
+                description: $data->get('seo_description') ?? '',
+                focusKeyword: $data->get('seo_keyword') ?? ''
+            );
+        }
+
+        $data->forget(['seo_title', 'seo_description', 'seo_keyword']);
+
+        $socials = ['facebook', 'twitter', 'instagram', 'linkedin', 'youtube', 'spotify'];
+        $profiles = $data->only($socials)->filter();
+
+        if ($profiles->isNotEmpty()) {
+            $user->setSocialProfiles($profiles);
+        }
+
+        $data->forget($socials);
+
+        $metadata = $data
+            ->filter(fn($value, $key) => str_starts_with($key, 'm:'))
+            ->mapWithKeys(fn($value, $key) => [substr($key, 2) => $value])
+            ->filter();
+
+        if ($metadata->isNotEmpty()) {
+            $user->setMetadata($metadata);
+        }
+
+        $data->forget($data->filter(fn($value, $key) => str_starts_with($key, 'm:'))->keys());
     }
 }
